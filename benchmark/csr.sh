0d845cae9b69494a4978dc4c010d4a594433a7f1
Jonas Primbs <jonas.primbs@uni-tuebingen.de>
2024-09-03 13:26:25 +0200

    cleanup benchmark

--- benchmark/csr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Generate Key Pair and Certificate Signing Request
-openssl req -new -newkey rsa:4096 -nodes -sha256 -config /app/csr.conf -keyout /app/output/ev.key -out /app/output/request.csr 2>/dev/null
