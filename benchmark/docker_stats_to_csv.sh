cd91e9b26afe8926135cd229e0de3aca06c46536
Jonas Primbs <jonas.primbs@uni-tuebingen.de>
2024-09-04 13:58:30 +0200

    add docker stats script

---
+++ benchmark/docker_stats_to_csv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUT_FILE=$1
+if [ -z "${OUT_FILE}" ]
+then
+  OUT_FILE="stats.csv"
+fi
+
+while true
+do
+  docker stats --no-stream --format "table {{.Container}},{{.Name}},{{.ID}},{{.CPUPerc}},{{.MemUsage}},{{.NetIO}},{.BlockIO}" >> $OUT_FILE
+  sleep 1
+done
